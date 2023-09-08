0ccb316c4ed116df75f1a451845edc55fcbc5e2f
Muhammad Ali <139394180+RaeezAli@users.noreply.github.com>
2023-09-08 10:09:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,169 @@
+//? Chapter Node.5
+
+// TODO Question Node.01
+
+// var sum;
+// sum = 5+3 ;
+// document.write("The Sum of 5 + 3 = "+sum)
+
+// TODO Question Node.02
+
+//! Subtraction
+
+// var sub;
+// sub = 5-3 ;
+// document.write("The Sum of 5 - 3 = "+sub)
+
+//! Multiplication
+
+// var multi;
+// multi = 5*3 ;
+// document.write("The Sum of 5 * 3 = "+multi)
+
+//! Division 
+
+// var divide;
+// divide = 5/3 ;
+// document.write("The Sum of 5 / 3 = "+divide)
+
+//! Modulus
+
+// var mod;
+// mod = 5%3 ;
+// document.write("The Sum of 5 % 3 = "+mod)
+
+// TODO Question Node.03
+
+// var num ;
+// document.write("Value after variable decleration is : "+num+"<h1>\n</h1>")
+
+// num = 5 ;
+// document.write("Initial Value is : "+num+"<h1>\n</h1>")
+
+// num++ ;
+// document.write("Value After Increment is : "+ num+"<h1>\n</h1>")
+
+// num = num+7 ;
+// document.write("Value after Addition is : "+ num+"<h1>\n</h1>")
+
+// num-- ;
+// document.write("Value After Decrement is : "+ num+"<h1>\n</h1>")
+
+// num = num % 3 ;
+// document.write("The Reminder is : "+ num+"<h1>\n</h1>")
+
+// TODO Question Node.04
+
+// var price , total;
+// price = 600 ;
+// total = price*5 ;
+// document.write("Total Cost to buy 5 Tickets to a Movie is "+ total +"PKR")
+
+// TODO Question Node.06
+
+// var C , F , celsius , Fahrenheit ;
+
+// C = 25 ;
+// F + 70 ;
+
+// celsius = (F - 32) * 5 / 9;
+// Fahrenheit = (C * 9 / 5) + 32;
+
+// document.write("25°C is "+ Fahrenheit +"°F"+"<h1>\n</h1>")
+// document.write("70°F "+ celsius +"°C")
+
+// TODO Question Node.07
+
+// var item1 , item2 , order1 , order2 , shipping , total;
+// item1 = 650 ;
+// item2 = 100 ;
+// order1 = 3 ;
+// order2 = 7 ;
+// shipping = 100 ;
+
+// total = (item1*order1)+(item2*order2)+shipping ;
+
+// document.write("<h1>Shopping Cart\n\n</h1>")
+
+// document.write("Price of Item 1 is "+ item1 +"<h1>\n</h1>")
+// document.write("Quantity of Item 1 is "+ order1 +"<h1>\n</h1>")
+// document.write("Price of Item 2 is "+ item2 +"<h1>\n</h1>")
+// document.write("Quantity of Item 2 is "+ order2 +"<h1>\n</h1>")
+// document.write("Shipping Charges "+ shipping +"<h1>\n\n</h1>")
+
+// document.write("Total Cost of Your Order is "+ total)
+
+// TODO Question Node.08
+
+// var total_marks , obtain_marks , percentage ;
+// total_marks = 100 ;
+// obtain_marks = 81 ;
+// percentage = (obtain_marks/total_marks) * 100 ;
+
+// document.write("<h1>Mark Sheet\n\n</h1>")
+
+// document.write("Total Marks : "+ total_marks+"<h1>\n</h1>")
+// document.write("Obtained Marks : "+ obtain_marks+"<h1>\n</h1>")
+// document.write("Percentage : "+ percentage + "%")
+
+// TODO Question Node.09
+
+// var us , riyal , total ;
+// us = 104.80;
+// riyal = 28;
+// total = (us*10)+(riyal*25)
+
+// document.write("<h1>Currency in PKR\n\n</h1>")
+
+// document.write("Total Currency is : "+ total)
+
+// TODO Question Node.10
+
+// var num = 6 ;
+
+// num = num + 5 * 10 / 2 ;
+
+// TODO Question Node.11
+
+// var currentYear , bornYear , age ;
+// currentYear = 2023 ;
+// bornYear = 2003 ;
+// age = currentYear-bornYear ;
+
+// document.write("<h1>Age Calculator\n\n</h1>")
+
+// document.write("Current Year : "+ currentYear+"<h1>\n</h1>")
+// document.write("Birth Year : "+ bornYear+"<h1>\n</h1>")
+// document.write("Your Age is : "+ age)
+
+// TODO Question Node.12
+
+// var radius , pi , Circumference , area ;
+// radius = 20 ;
+// pi = 3.142 ;
+// Circumference = 2*pi*radius;
+// area = pi*radius*radius ;
+
+
+// document.write("<h1>The Geometrizer\n\n</h1>")
+
+// document.write("Radius of Circle : "+ radius+"<h1>\n</h1>")
+// document.write("The Circumference is : "+ Circumference+"<h1>\n</h1>")
+// document.write("The Area is : "+ area)
+ 
+// TODO Question Node.13
+
+// var snack , currentAge , maxAge , perDay , total;
+// snack = "Wavy";
+// currentAge = 20 ;
+// maxAge = 65 ;
+// perDay = 3 ;
+// total = (maxAge-currentAge)*3;
+
+// document.write("<h1>The Lifetime Supply Calculator\n\n</h1>")
+
+// document.write("Favourite Snack : "+snack+"<h1>\n</h1>")
+// document.write("Current Age : "+currentAge+"<h1>\n</h1>")
+// document.write("Estimated Maximum Age : "+maxAge+"<h1>\n</h1>")
+// document.write("Amount of Snack per day : "+perDay+"<h1>\n</h1>")
+// document.write("You will need "+total+ " " +snack+" to last you until the ripe old age of "+maxAge)
